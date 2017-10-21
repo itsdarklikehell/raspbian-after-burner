0de8ba0917dac086627fb72e057aa85fd4c5a64b
bauke <bauke.molenaar@gmail.com>
2017-10-21 04:19:08 +0200

    new file:   After-Burner.sh

---
+++ After-Burner.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+UPGR8(){
+#	update and upgrade system first, pretty obvious.
+echo "update and upgrade system"
+  sudo apt-get update
+  sudo apt-get upgrade -y
+}
+
+RASPICONF(){
+raspi-config
+}
+
+ENSSH(){
+sudo touch /boot/ssh
+}
+
+LOCL(){
+#	update localization
+echo "update locale"
+update-locale
+}
+
+RETROPIE(){
+#	clone retropie setup git
+sudo apt-get install git lsb-release -y
+cd
+#cd git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+}
+
+RETROPIESETUP(){
+cd RetroPie-Setup
+chmod +x retropie_setup.sh
+sudo ./retropie_setup.sh
+}
+
+AWSMRETRPIBGM(){
+#	Awesome retropie emulationstation background music
+#	for more instructions please read:
+#	https://retropie.org.uk/forum/topic/347/background-music-continued-from-help-support
+#
+sudo apt-get install wget python-pygame
+cd
+mkdir PyScript
+cd PyScript
+wget https://pastebin.com/raw/7E9JiZGQ
+cat 7E9JiZGQ bgm-v103.py
+sudo nano bgm-v103.py
+echo "Run 'sudo nano /etc/rc.local'"
+echo "Above exit 0, put the following code: '(sudo python /home/pi/PyScripts/bgm-v103.py) &'"
+}
+
+CRE8AP(){
+#	install create_ap
+sudo apt-get install curl git bash util-linux procps hostaptd iproute iw haveged dnsmasq iptables -y
+cd
+#	curl installer method
+#curl -Ls https://github.com/itsdarklikehell/create_ap/raw/master/install.sh
+#	git clone method
+#git clone https://github.com/itsdarklikehell/create_ap
+#cd create_ap
+#sudo make install
+#sudo nano /etc/create_ap.conf   #	edit the config
+#sudo systemctl start create_ap  #	start the hotspot
+#sudo systemctl enable create_ap #	set to enable at boot
+}
+
+EMBY(){
+
+# Currently we support armv7/armhf, aarch64 for arm architectures. 
+
+#Recommend install method for armhf/armv7 (Tested rpi2/3 running rasbian): 
+#curl -sSL https://get.docker.com/ | sh docker run -it --rm \ --volume /usr/local/bin:/target \ emby/embyserver:armv7 instl 
+
+#Recommend install method for aarch64/armv8 (tested on openSUSE Tumbleweed): 
+#docker run -it --rm \ --volume /usr/local/bin:/target \ emby/embyserver:aarch64 instl 
+
+#Installing directly on raspbian or other debian directive without docker (armhf/armv7/aarch64): 
+wget -qO - http://download.opensuse.org/repositories/home:emby/xUbuntu_14.04/Release.key | sudo apt-key add - sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/emby/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/emby-server.list"
+sudo apt-get update
+sudo apt-get install emby-server
+
+
+#Getting ffmpeg: https://www.johnvansickle.com/ffmpeg/ openSUSE installation: 1. Install an openSUSE image for your corresponding board: Full Index: https://en.opensuse.org/Portal:ARM RPI2: https://en.opensuse.org/HCL:Raspberry_Pi2 RPI3: https://en.opensuse.org/HCL:Raspberry_Pi3 2. Add emby repo. https://software.opensuse.org/download.html?project=home%3Aemby&package=emby-server 3. Install. sudo zypper in emby-server 
+
+}
+
+
+UPGR8
+RASPICONFIG
+ENSSH
+LOCL
+RETROPIE
+RETROPIESETUP
+AWSMRETRPIBGM
+CRE8AP
+EMBY
